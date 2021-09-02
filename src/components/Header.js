ddc9fe8038e5963612762df0fd85905fe0822594
hugues0 <ntwari.hugues@gmail.com>
2021-09-02 13:21:41 +0200

    top menu done

---
+++ src/components/Header.js
@@ -0,0 +1,22 @@
+const Header = () => {
+    return (
+      <header>
+        <nav className="container flex items-center py-4 mt-4 sm:mt-12">
+          <div className="py-1">
+            <img src="./assets/images/logo-bookmark.svg" alt="logo" />
+          </div>
+          <ul className="hidden sm:flex flex-1 justify-end items-center gap-12 text-bookmark-blue uppercase text-xs">
+            <li className="cursor-pointer ">Features</li>
+            <li className="cursor-pointer ">Pricing</li>
+            <li className="cursor-pointer ">Contact</li>
+            <button type="button" className="bg-bookmark-red text-white rounded-md px-7 py-3 uppercase">Login</button>
+          </ul>
+          <div className="flex sm:hidden flex-1 justify-end">
+            <i className="fas fa-bars text-xl"></i>
+          </div>
+        </nav>
+      </header>
+    );
+}
+
+export default Header
