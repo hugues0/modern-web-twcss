cf289d8d6f6af7d375b7345f4fe9018aff965b0d
hugues0 <ntwari.hugues@gmail.com>
2021-09-02 15:44:36 +0200

    features section done

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header';
 import Hero from './components/Hero';
+import Features from './components/Features';
 
 function App() {
   return (
     <div className="font-Poppins">
       <Header/>
       <Hero />
+      <Features />
     </div>
   );
 }
